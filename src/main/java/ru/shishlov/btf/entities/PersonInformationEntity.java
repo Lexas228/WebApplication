f66fd1ab8c7dfea1d3f48954dfb079822d17f680
Dmityi Shishlov <dima.shishlov89@mail.ru>
2021-10-13 20:15:12 +0300

    Deleted checks from entity class

--- src/main/java/ru/shishlov/btf/entities/PersonInformationEntity.java
+++ src/main/java/ru/shishlov/btf/entities/PersonInformationEntity.java
@@ -18,22 +18,23 @@ public class PersonInformationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Size(min = 2, max = 100)
+
     @Column(name = "name")
     private String name;
-    @Size(min = 2, max = 100)
+
     @Column(name = "surname")
     private String surname;
-    @NotNull
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+
     @Temporal(TemporalType.DATE)
     @Column(name = "birthday")
     private Date birthday;
+
     @Column(name = "information")
     private String information;
-    @Size(min = 2, max = 100)
+
     @Column(name = "address")
     private String address;
+
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "person_id", nullable = false)
     private PersonEntity person;
