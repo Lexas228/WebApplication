1a7b5ea965f831785fe61521df1617aa79ddf399
OMGCool <dima.shishlov89@mail.ru>
2021-10-14 22:12:26 +0300

    deleted checks

--- src/main/java/ru/shishlov/btf/entities/PersonEntity.java
+++ src/main/java/ru/shishlov/btf/entities/PersonEntity.java
@@ -23,15 +23,12 @@ public class PersonEntity {
     @Column(name = "id")
     private long id;
 
-    @Size(min = 2, max = 100)
     @Column(name = "login")
     private String login;
 
-    @Size(min = 2, max = 100)
     @Column(name = "password")
     private String password;
 
-    @NotNull
     @OneToOne(mappedBy = "person", cascade = CascadeType.ALL, optional = false, fetch = FetchType.EAGER)
     private PersonInformationEntity personInformation;
 
